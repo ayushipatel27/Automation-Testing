c61a96ee6fdb08b6295c17704cb80a70f2fd6edd
Ayushi Vipul Patel <ayushi27@uab.edu>
2018-05-15 14:08:02 -0500

    Set up project.

---
+++ src/tests/DropDownSelectionTest.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class DropDownSelectionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/tests/NavigateToTest.java
@@ -0,0 +1,9 @@
+package tests;
+
+public class NavigateToTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
