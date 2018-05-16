5d182fdef8106bd5ce828101ba6bd0c162406fa9
Ayushi Vipul Patel <ayushi27@uab.edu>
2018-05-15 19:30:20 -0500

    Trying to wait to locate elements but it's still breaking..

--- src/tests/TestCases.java
+++ src/tests/TestCases.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestCases {
 	private static WebDriver driver;
@@ -12,13 +14,11 @@ public class TestCases {
 	public static void main(String[] args) throws InterruptedException {
 		navigateTo("https://developers.google.com/");
 		confirmText("Hone your skills on the latest cloud technologies with Google experts at hundreds of breakout sessions and interactive on-demand hands-on labs and bootcamps. You'll have the opportunity to engage with the best minds in cloud technology on how your industry is adapting, innovating, and growing with cloud.", "//*[@id='gc-wrapper']/div[2]/article/article/section[1]/div/div[1]/div/div[1]/p[1]");
-		clickButton("//*[@id=\"gc-wrapper\"]/div[2]/article/article/section[1]/div/div[1]/div/a");
-		driver.close();
-		navigateTo("https://cloud.withgoogle.com/next18/sf/?utm_source=devsite&utm_medium=hpp&utm_campaign=cloudnext_april18");
-		confirmText("Imagine", "//*[@id=\"main\"]/span/div[2]/div/div/div[1]/div[1]/div/div[1]/div[2]/h3");
+		clickButton("//*[@id=\'gc-wrapper\']/div[2]/article/article/section[1]/div/div[1]/div/a");
+		confirmText("Imagine", "//*[@id=\'main\']/span/div[2]/div/div/div[1]/div[1]/div/div[1]/div[2]/h3");
+		clickButton("//*[@id=\'main\']/span/div[1]/div/div/div/div[3]/div[2]/div/button");
 		sendKeys();
 		selectFromDropDown();
-		driver.close();
 	}
 	
 	public static void navigateTo(String url) throws InterruptedException {
@@ -40,7 +40,7 @@ public class TestCases {
 		
 		// Navigate to URL
 		driver.get(url);
-				
+		
 		System.out.println("Successfully navigated to url: " + url + "\n\n");
 		
 	}
@@ -48,43 +48,62 @@ public class TestCases {
 	public static void confirmText(String text, String xpath) {
 		System.out.println("Trying to confirm that given string is equal to the text on page.");
 		WebElement element = driver.findElement(By.xpath(xpath));
+		waitToLocate(xpath);
 		System.out.println("Test case: " + text);
 		System.out.println("Result: " + element.getText());
 		if (element.getText() == text) {
-			System.out.println("Equals.");
+			System.out.println("\nEquals.");
 		}
 		else {
-			System.out.println("Does not equals.");
+			System.out.println("\nDoes not equals.");
 		}
 		System.out.println("\n\n");
 	}
 	
+	
 	public static void clickButton(String xpath) {
 		System.out.println("Trying to click a link.");
-		WebElement element = driver.findElement(By.xpath(xpath));
+		WebElement element = null;
+		try {
+		element = driver.findElement(By.xpath(xpath));
+		waitToLocate(xpath);
+		}
+		catch (Exception e) {
+			System.out.println("Cannot find element.");
+		}
 		element.click();
 		System.out.println("Succesfully clicked the link." + "\n\n");
 
 	}
 	
 	public static void sendKeys() {
-		WebElement firstname = driver.findElement(By.xpath("//*[@id=\"firstName\"]"));
+		WebElement firstname = driver.findElement(By.id("firstName"));
+		waitToLocate("//*[@id=\'firstName\']");
 		firstname.sendKeys("Ayushi");
-		WebElement lastname = driver.findElement(By.xpath("//*[@id=\"firstName\"]"));
+		WebElement lastname = driver.findElement(By.xpath("//*[@id=\'lastName\']"));
+		waitToLocate("//*[@id=\'lastName\']");
 		lastname.sendKeys("Patel");
-		WebElement email = driver.findElement(By.xpath("//*[@id=\"firstName\"]"));
+		WebElement email = driver.findElement(By.xpath("//*[@id=\'email\']"));
+		waitToLocate("//*[@id=\'email\']");
 		email.sendKeys("ayushiipatel27@gmail.com");
-		WebElement jobtitle = driver.findElement(By.xpath("//*[@id=\"firstName\"]"));
+		WebElement jobtitle = driver.findElement(By.xpath("//*[@id=\'jobTitle\']"));
+		waitToLocate("//*[@id=\'jobTitle\']");
 		jobtitle.sendKeys("QA Engineer");
-		WebElement company = driver.findElement(By.xpath("//*[@id=\"firstName\"]"));
+		WebElement company = driver.findElement(By.xpath("//*[@id=\'company\']"));
+		waitToLocate("//*[@id=\'company\']");
 		company.sendKeys("Influence Health");
 	}
 	
 	public static void selectFromDropDown() {
-		Select industry = new Select(driver.findElement(By.xpath("//*[@id=\"industry\"]")));
+		Select industry = new Select(driver.findElement(By.xpath("//*[@id=\'industry\']")));
 		industry.selectByValue("Education");
-		Select country = new Select(driver.findElement(By.xpath("//*[@id=\"country\"]")));
+		Select country = new Select(driver.findElement(By.xpath("//*[@id=\'country\']")));
 		country.selectByValue("United States");
+	}
+	
+	public static void waitToLocate(String xpath) {
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
 	}
 	
 }
