ec40b3207802092b94d4d416393c4cc0ad618fb6
Ayushi Vipul Patel <ayushi27@uab.edu>
2018-05-15 20:12:05 -0500

    Completed testing.

--- src/tests/TestCases.java
+++ src/tests/TestCases.java
@@ -12,15 +12,25 @@ public class TestCases {
 	private static WebDriver driver;
 	
 	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Test Case 1\n");
 		navigateTo("https://developers.google.com/");
+		System.out.println("Test Case 2\n");
 		confirmText("Hone your skills on the latest cloud technologies with Google experts at hundreds of breakout sessions and interactive on-demand hands-on labs and bootcamps. You'll have the opportunity to engage with the best minds in cloud technology on how your industry is adapting, innovating, and growing with cloud.", "//*[@id='gc-wrapper']/div[2]/article/article/section[1]/div/div[1]/div/div[1]/p[1]");
+		System.out.println("Test Case 3\n");
 		clickButton("//*[@id=\'gc-wrapper\']/div[2]/article/article/section[1]/div/div[1]/div/a");
+		System.out.println("Test Case 4\n");
 		confirmText("Imagine", "//*[@id=\'main\']/span/div[2]/div/div/div[1]/div[1]/div/div[1]/div[2]/h3");
+		System.out.println("Test Case 5\n");
 		clickButton("//*[@id=\'main\']/span/div[1]/div/div/div/div[3]/div[2]/div/button");
+		System.out.println("Test Case 6\n");
 		sendKeys();
 		selectFromDropDown();
 	}
-	
+	/**
+	 * This method navigating to given url on Chrome
+	 * @param url : the url that user is trying to navigate
+	 * @throws InterruptedException
+	 */
 	public static void navigateTo(String url) throws InterruptedException {
 		System.out.println("Trying to navigate to url: " + url);
 
@@ -45,13 +55,18 @@ public class TestCases {
 		
 	}
 	
+	/**
+	 * This method checks if the given string matches the string from the element located on the web page."
+	 * @param text : given string
+	 * @param xpath : xpath used to locate element
+	 */
 	public static void confirmText(String text, String xpath) {
 		System.out.println("Trying to confirm that given string is equal to the text on page.");
-		WebElement element = driver.findElement(By.xpath(xpath));
 		waitToLocate(xpath);
+		WebElement element = driver.findElement(By.xpath(xpath));
 		System.out.println("Test case: " + text);
 		System.out.println("Result: " + element.getText());
-		if (element.getText() == text) {
+		if (element.getText().equals(text)) {
 			System.out.println("\nEquals.");
 		}
 		else {
@@ -60,50 +75,93 @@ public class TestCases {
 		System.out.println("\n\n");
 	}
 	
-	
+	/**
+	 * This method tries to click a link or a button on the webpage
+	 * @param xpath : xpath used to locate element
+	 */
 	public static void clickButton(String xpath) {
-		System.out.println("Trying to click a link.");
+		System.out.println("Trying to click a link.\n");
 		WebElement element = null;
 		try {
-		element = driver.findElement(By.xpath(xpath));
 		waitToLocate(xpath);
+		element = driver.findElement(By.xpath(xpath));
+		System.out.println("Link: " + element.getText());
 		}
 		catch (Exception e) {
 			System.out.println("Cannot find element.");
 		}
 		element.click();
-		System.out.println("Succesfully clicked the link." + "\n\n");
+		System.out.println("\nSuccesfully clicked the link." + "\n\n");
 
 	}
 	
+	/**
+	 * This method sends keys to the input tag
+	 */
 	public static void sendKeys() {
-		WebElement firstname = driver.findElement(By.id("firstName"));
+		System.out.println("Trying to send keys.");
+		
 		waitToLocate("//*[@id=\'firstName\']");
+		WebElement firstname = driver.findElement(By.xpath("//*[@id=\'firstName\']"));
 		firstname.sendKeys("Ayushi");
-		WebElement lastname = driver.findElement(By.xpath("//*[@id=\'lastName\']"));
+		System.out.println("Firstname sent.");
+		
+		
 		waitToLocate("//*[@id=\'lastName\']");
+		WebElement lastname = driver.findElement(By.xpath("//*[@id=\'lastName\']"));
 		lastname.sendKeys("Patel");
-		WebElement email = driver.findElement(By.xpath("//*[@id=\'email\']"));
+		System.out.println("Lastname sent.");
+		
+		
 		waitToLocate("//*[@id=\'email\']");
+		WebElement email = driver.findElement(By.xpath("//*[@id=\'email\']"));
 		email.sendKeys("ayushiipatel27@gmail.com");
-		WebElement jobtitle = driver.findElement(By.xpath("//*[@id=\'jobTitle\']"));
+		System.out.println("Email sent.");
+		
+		
 		waitToLocate("//*[@id=\'jobTitle\']");
+		WebElement jobtitle = driver.findElement(By.xpath("//*[@id=\'jobTitle\']"));
 		jobtitle.sendKeys("QA Engineer");
-		WebElement company = driver.findElement(By.xpath("//*[@id=\'company\']"));
+		System.out.println("Job Title sent.");
+		
+		
 		waitToLocate("//*[@id=\'company\']");
+		WebElement company = driver.findElement(By.xpath("//*[@id=\'company\']"));
 		company.sendKeys("Influence Health");
+		System.out.println("Company sent.");
+		
+
+		System.out.println("Succesfully sent the keys." + "\n\n");
 	}
 	
+	/**
+	 * This method selects values from the dropdown 
+	 */
 	public static void selectFromDropDown() {
+		System.out.println("Trying to select from dropdown.");
+		
+		waitToLocate("//*[@id=\'industry\']");
 		Select industry = new Select(driver.findElement(By.xpath("//*[@id=\'industry\']")));
 		industry.selectByValue("Education");
+		System.out.println("Selection complete.");
+		
+		waitToLocate("//*[@id=\'country\']");
 		Select country = new Select(driver.findElement(By.xpath("//*[@id=\'country\']")));
-		country.selectByValue("United States");
+		country.selectByValue("US");
+		System.out.println("Selection complete.");
+
+		
+		
+		System.out.println("Succesfully selected values from dropdown." + "\n\n");
 	}
 	
+	/**
+	 * This method waits for web driver to look for an element 
+	 * @param xpath : xpath used to locate element
+	 */
 	public static void waitToLocate(String xpath) {
 		WebDriverWait wait = new WebDriverWait(driver, 30);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
 	}
 	
 }
